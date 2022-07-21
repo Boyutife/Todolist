a8922c73541eb3c8d8a02200f9bf10f9c034f23e
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2022-07-21 14:01:44 +0100

    another way to add a new todo

--- todo.js
+++ todo.js
@@ -2,19 +2,27 @@ const addForm = document.querySelector('.add');
 
 const add = document.querySelector('.todolist');
 
-addForm.addEventListener('submit', e =>{
-
-  e.preventDefault();
-  const todo = addForm.add.value.trim();
+const newTemplate = todo =>{
 
   const html = `
   <li class="list-group-item d-flex justify-content-between align-items-center">
     <span>${todo}</span>
     <i class="far fa-trash-alt delete"></i>
   </li>
-`;
+  `;
 
   add.innerHTML += html;
 
-  
+};
+
+
+
+addForm.addEventListener('submit', e =>{
+
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  newTemplate(todo);
+
+
 });
